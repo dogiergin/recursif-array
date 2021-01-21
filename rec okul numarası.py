d78828b98ec7be61c10bf03435aeaedad96c683a
dogiergin <73343022+dogiergin@users.noreply.github.com>
2021-01-21 22:27:14 +0300

    Update rec okul numarası.py

--- rec okul numarası.py
+++ rec okul numarası.py
@@ -14,7 +14,7 @@ def reclen(arr):
     adim += 1
     return reclen(arr)
 
-"""burda adım sayısı  aslında sizin gösteridğiniz örnekteki 1 + fonk. çağrısına eşdeğer"""
+
 def sum(mut):
     toplam = 0
     for i in range(len(mut) - 2, -1, -1):
@@ -24,14 +24,3 @@ def sum(mut):
 
 sum(liste)
 reclen(liste)
-
-
-"""func([a,b,c,d,e,f])                                          6         
-    1 + func([b,c,d,e,f])                                      5
-        1 + func([c,d,e,f])                                   4
-            1 + func([d,e,f])                                3
-                1 + func([e,f])                             2
-                    1 + func([f])                          1     
-                        1 + 0             sıfırı bulduktan sonra yukarı doğru toplayarak çıkar  
-
-eğer kodu denemek isterseniz github hesabımda olacaktır """
